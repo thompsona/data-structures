c34cff39e2c7e12906d07a5a2c030a4745cb2123
thompsona <athomp@hawaii.edu>
2014-02-26 12:36:30 -0800

    functional queue.js and stack.js pass specs

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    ++size;
+    for(var i = size; i > 0; --i) {
+        storage[i] = storage[i-1];
+    }
+    storage[0] = value;
   };
 
   instance.dequeue = function(){
+    if(size > 0) {
+      var temp = storage[--size];
+      delete storage[size];
+      return temp;
+    }
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,24 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   instance.push = function(value){
+    storage[size] = value;
+    ++size;
   };
 
   instance.pop = function(){
+    if(size > 0) {
+      var temp = storage[size-1];
+      --size;
+      return temp;
+    }
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
